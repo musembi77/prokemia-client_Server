0925c02c98efea6001911739a19d9644d284dc08
dennissammy77 <dennissammy77@gmail.com>
2022-12-31 13:52:23 +0300

    added url for client access

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ db.connect()
 const app = express()
 app.use(express.json())
 
+// app.use(cors({credentials:true, 
+//     origin: 'http://localhost:3000' }));
 app.use(cors({credentials:true, 
-    origin: 'http://localhost:3000' }));
+    origin: 'https://prokemia-client-web.vercel.app/' }));
 //imports
 
 /*--accounts---*/
