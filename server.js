66be65739cb0547ab83a18b650654e13a35b26b9
dennissammy77 <dennissammy77@gmail.com>
2022-12-31 22:48:17 +0300

    added url for client access

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = require("./app.js");
 require("dotenv").config();
 
 const server = http.createServer(app);
-const host = '0.0.0.0';
-const port = process.env.Port;
+let port = process.env.Port || 8080;
 
 server.listen(port, (req,res)=>{
 	console.log(`server listening on http://localhost:${port}`)
