2496dfb898b93f63f359d0ac61e80f6320035a73
dennissammy77 <dennissammy77@gmail.com>
2023-01-01 16:03:54 +0300

    changed Port to PORT in server.js and .env

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = require("./app.js");
 require("dotenv").config();
 
 const server = http.createServer(app);
-let port = process.env.PORT || 5000;
+const port = process.env.Port || 5000;
+//const port = process.env.Port || 5001
 
 server.listen(port, (req,res)=>{
 	console.log(`server listening on http://localhost:${port}`)
