24f3afb1788112c6791f6ba4d0347bdaf4098d94
musembi sammy <79364582+musembi77@users.noreply.github.com>
2023-06-16 14:06:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ let origins = ['https://prokemia-client-web-musembi77.vercel.app',
 				'http://localhost:3000',
 				'https://prokemia-client-web.vercel.app',
 				'https://client-frontend-two.vercel.app',
-				'https://prokemia.com/'];
+	       			'https://www.prokemia.com/',
+	       'https://www.prokemia.com/',
+	       'https://prokemia.com/',
+				'https://prokemia.com'];
 app.use(cors({credentials:true, origin: origins}));
 //imports
 
